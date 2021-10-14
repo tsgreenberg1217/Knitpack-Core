d4c307092ec4f11cb5d1f7ac2d35fccdc7321c9e
Todd Greenberg <tsgreenberg1217@gmail.com>
2021-10-14 00:03:14 -0400

    removed callbacks

--- src/main/java/com/tgreenberg/core/api/KnitPackApi.kt
+++ src/main/java/com/tgreenberg/core/api/KnitPackApi.kt
@@ -4,22 +4,21 @@ import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
 import com.tgreenberg.core.models.ProjectTxt
 
-object KnitPackApi{
+object KnitPackApi {
 
-    fun postProject(project: ProjectTxt){
-//        val db = Firebase.firestore
-//        val postProject = hashMapOf(
-//            project.name to "name",
-//            Gson().toJson(project.steps) to "steps",
-//            project.level to "level"
-//        )
-//        db.collection("projects")
-//            .add(postProject)
-//            .addOnSuccessListener {
-//                Log.d("FIREBASE TESTING", "success $it")
-//            }
-//            .addOnFailureListener {
-//                Log.d("FIREBASE TESTING", "error $it")
-//            }
+    suspend fun postProject(project: ProjectTxt) {
+        val db = Firebase.firestore
+        val postProject = hashMapOf(
+            project.name to "name",
+            Gson().toJson(project.steps) to "steps",
+            project.level to "level"
+        )
+        val x = db.collection("projects").add(postProject)
+
+        if (x.isSuccessful) {
+            Log.d("FIREBASE TESTING", "success ${x.result}")
+        } else {
+            Log.d("FIREBASE TESTING", "error ${x.exception?.message}")
+        }
     }
 }
