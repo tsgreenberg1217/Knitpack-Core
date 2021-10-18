f211959dbdc8993df4894b3e7fb412fbe355897f
Todd Greenberg <tsgreenberg1217@gmail.com>
2021-10-17 22:33:56 -0400

    kotlin dsl 2

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+apply {
+    from("$rootDir/${Android.androidLibrary}")
+}
+
+
+dependencies {
+    "implementation"(platform(Google.firebaseBom))
+    "implementation"(Google.firestore)
+    "implementation"(Google.analytics)
+}
