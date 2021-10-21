af3e607eb9dd9f8a282cfcdcf796653a6dc5b1d8
Todd Greenberg <tsgreenberg1217@gmail.com>
2021-10-21 01:36:08 -0400

    added image object

--- src/main/java/com/tgreenberg/core/models/apiModels.kt
+++ src/main/java/com/tgreenberg/core/models/apiModels.kt
@@ -1,14 +1,26 @@
 package com.tgreenberg.core.models
 
+import android.net.Uri
+
 data class ProjectTxt(
     val name: String, val steps: List<String>, val level: String
 )
 
+data class KnitUri(var res: Uri?)
+
 data class UIKnittingProject(
     var name: String = "",
     var notes: String = "",
-    var description:String = "",
+    var description: String = "",
     var pattern: String = "",
-    var yarn:String = "",
-    var needles:String = ""
+    var yarn: String = "",
+    var needles: String = "",
+    var images: List<KnitUri> = listOf(
+        KnitUri(null),
+        KnitUri(null),
+        KnitUri(null),
+        KnitUri(null),
+        KnitUri(null)
+    )
+
 )
